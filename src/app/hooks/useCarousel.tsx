558beb005c2bafb92220fe5d92162a6267963b2d
martinet <remymartinet.contact@gmail.com>
2024-10-04 20:45:24 +0200

    Feat: Added ContextProvider in layout.tsx to manage shared props across different pages

---
+++ src/app/hooks/useCarousel.tsx
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react";
+
+interface CarouselProps {
+  carouselIndex: number;
+  handleNextSlide: () => void;
+  handlePrevSlide: () => void;
+}
+
+const useCarousel = (items: string[]): CarouselProps => {
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  const handleNextSlide = useCallback(() => {
+    setCarouselIndex((prevIndex) => (prevIndex + 1) % (items?.length || 0));
+  }, [items]);
+
+  const handlePrevSlide = useCallback(() => {
+    setCarouselIndex((prevIndex) =>
+      prevIndex === 0 ? (items?.length || 0) - 1 : prevIndex - 1,
+    );
+  }, [items]);
+
+  return {
+    carouselIndex,
+    handleNextSlide,
+    handlePrevSlide,
+  };
+};
+
+export default useCarousel;
