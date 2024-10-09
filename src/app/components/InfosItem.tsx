edeb23277f7680ab7726b0120dc087af3df98943
martinet <remymartinet.contact@gmail.com>
2024-10-10 01:09:20 +0200

    Refactor InfosItem component to be reusable with different classes in various parts of the code

--- src/app/components/InfosItem.tsx
+++ src/app/components/InfosItem.tsx
@@ -4,9 +4,11 @@ interface InfosItemProps {
   className?: string;
 }
 
-const InfosItem = ({ label, value }: InfosItemProps) => {
+const InfosItem = ({ label, value, className }: InfosItemProps) => {
   return (
-    <div className="flex w-full flex-row items-center justify-between gap-4">
+    <div
+      className={`flex w-full flex-row items-center justify-between gap-4 ${className}`}
+    >
       <span className="text-md md:text-xl">{label}</span>
       <p className="opacity-70">{value}</p>
     </div>
