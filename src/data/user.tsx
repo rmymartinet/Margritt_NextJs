5a05a684c4c1a625f6e5a47da89f7e64a0b26cbc
martinet <remymartinet.contact@gmail.com>
2024-10-10 12:21:39 +0200

    Update TikTok link

--- src/data/user.tsx
+++ src/data/user.tsx
@@ -2,5 +2,5 @@ export const CONTACT_INFO = {
   email: "margrittmartinet@gmail.com",
   number: "+33.7.83.40.51.05",
   instagram: "https://www.instagram.com/maargriitt/",
-  tiktok: "https://www.tiktok.com/@margrittus?lang=fr",
+  tiktok: "https://www.tiktok.com/@margriitt?is_from_webapp=1&sender_device=pc",
 };
