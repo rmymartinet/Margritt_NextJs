432c80a8c7034d8b7a77a84ee38fe53d18cb760b
martinet <remymartinet.contact@gmail.com>
2024-10-10 18:38:58 +0200

    unlock api routes for test to production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/admin(.*)",
-  "/home",
-  "/api(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/admin(.*)"]);
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
 });
