4cec0615c6fc07014a386482fbc876bd4ae3676d
martinet <remymartinet.contact@gmail.com>
2024-10-11 10:01:15 +0200

    Trying to fix the Skeleton path by using a relative one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import "../app/globals.css";
 import Transition from "./components/Animations/pagetransition/PageTransition";
@@ -22,13 +23,24 @@ export default function RootLayout({
     document.title = "Margritt.com";
   }, []);
 
+  const pathname = usePathname();
+
+  const isGallery = /^\/gallery\/[^\/]+$/.test(pathname);
+
   return (
     <>
       <ClerkProvider>
         <html lang="fr">
           <body className={`relative p-2 antialiased md:px-[4vw]`}>
             <motion.div>
-              {isMounted && (width <= 1024 ? <MobileNav /> : <Nav />)}
+              {isMounted &&
+                (!isGallery ? (
+                  width <= 1024 ? (
+                    <MobileNav />
+                  ) : (
+                    <Nav />
+                  )
+                ) : null)}{" "}
               <CartProvider>
                 <Transition>{children}</Transition>
                 {isMounted && <CartSideBar />}
