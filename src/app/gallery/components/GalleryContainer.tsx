25336500da0b982fd071c9948cf32dd6a6879f69
rmymartinet <remymartinet.contact@gmail.com>
2024-10-24 12:55:48 +0200

    Refactor: Update GalleryContainer component to conditionally render Link and Image based on filter prop

--- src/app/gallery/components/GalleryContainer.tsx
+++ src/app/gallery/components/GalleryContainer.tsx
@@ -3,21 +3,25 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface ImagesContainerProps {
+  filter: string;
   item: Item[];
   isCursorPointer?: boolean;
   isOriginal?: boolean;
 }
 
-const GalleryContainer = ({ item, isCursorPointer }: ImagesContainerProps) => {
+const GalleryContainer = ({
+  filter,
+  item,
+  isCursorPointer,
+}: ImagesContainerProps) => {
   return (
-    <section className="flex h-screen justify-center">
+    <section className="flex justify-center">
       {/*AJOUT DE HEROSUBCONTENT*/}
       <div className="md:max-h-screen-sm flex flex-col gap-40 md:max-w-screen-sm">
         {item.map((imgData: Item, id: number) => (
           <div className="flex flex-col gap-10" key={id}>
-            <Link href={`/gallery/${imgData.id}`}>
+            {filter === "gallery" ? (
               <Image
-                className={`${isCursorPointer && "cursor-pointer"}`}
                 width={800}
                 height={800}
                 layout="responsive"
@@ -25,7 +29,19 @@ const GalleryContainer = ({ item, isCursorPointer }: ImagesContainerProps) => {
                 src={imgData.imageUrls[0]}
                 alt="Image"
               />
-            </Link>
+            ) : (
+              <Link href={`/gallery/${imgData.id}`}>
+                <Image
+                  className={`${isCursorPointer && "cursor-pointer"}`}
+                  width={800}
+                  height={800}
+                  layout="responsive"
+                  objectFit="contain"
+                  src={imgData.imageUrls[0]}
+                  alt="Image"
+                />
+              </Link>
+            )}
             <div className="flex flex-col items-start md:flex-row md:items-center md:justify-between md:gap-0 md:px-10">
               <div className="flex flex-col gap-6">
                 <div className="md:text-md flex flex-wrap gap-3 lg:text-lg">
