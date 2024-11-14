941297fe497299ffa3681b1f1a030acff951a53a
rmymartinet <remymartinet.contact@gmail.com>
2024-11-14 15:38:21 +0100

    Refactor: Update ArtworksContainer component to remove unnecessary code and fix image size issue and Add title animation

--- src/app/artworks/components/ArtworksContainer.tsx
+++ src/app/artworks/components/ArtworksContainer.tsx
@@ -4,47 +4,27 @@ import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
 
 interface ImagesContainerProps {
-  filter: string;
   item: Item[];
   isCursorPointer?: boolean;
   isOriginal?: boolean;
 }
 
-const ArtworksContainer = ({
-  filter,
-  item,
-  isCursorPointer,
-}: ImagesContainerProps) => {
+const ArtworksContainer = ({ item }: ImagesContainerProps) => {
   return (
     <section className="flex justify-center">
       {/*AJOUT DE HEROSUBCONTENT*/}
-      <div className="md:max-h-screen-sm flex flex-col gap-40 md:max-w-screen-sm">
+      <div className="md:max-h-screen-md flex flex-col gap-40 md:max-w-screen-sm">
         {item.map((imgData: Item, id: number) => (
           <div className="flex flex-col gap-10" key={id}>
-            {filter === "artworks" ? (
-              <Image
-                width={5000}
-                height={5000}
-                layout="responsive"
-                objectFit="contain"
-                src={imgData.imageUrls[0]}
-                alt="Image"
-              />
-            ) : (
-              <div className="flex flex-col gap-6">
-                <Link href={`/artworks/${imgData.id}`}>
-                  <Image
-                    className={`${isCursorPointer && "cursor-pointer"}`}
-                    width={5000}
-                    height={5000}
-                    layout="responsive"
-                    objectFit="contain"
-                    src={imgData.imageUrls[0]}
-                    alt="Image"
-                  />
-                </Link>
-              </div>
-            )}
+            <Image
+              width={5000}
+              height={5000}
+              layout="responsive"
+              objectFit="contain"
+              src={imgData.imageUrls[0]}
+              alt="Image"
+            />
+
             <div className="flex flex-col items-start md:flex-row md:items-center md:justify-between md:gap-0 md:px-10">
               <div className="flex w-full justify-between gap-6">
                 <div className="md:text-md flex flex-wrap gap-3 lg:text-lg">
@@ -54,16 +34,15 @@ const ArtworksContainer = ({
                   <span>|</span>
                   <p>{imgData.date}</p>
                 </div>
-                {filter === "project" && (
-                  <Link href={`/artworks/${imgData.id}`} className="self-end">
-                    <div className="group flex items-center gap-2 text-blue-500">
-                      <p className="cursor-pointer">Learn more</p>
-                      <div className="icon transition-all duration-200 ease-in-out group-hover:translate-x-1">
-                        <IoIosArrowForward />
-                      </div>
+
+                <Link href={`/artworks/${imgData.id}`} className="self-end">
+                  <div className="group flex items-center gap-2 text-blue-500">
+                    <p className="cursor-pointer">Learn more</p>
+                    <div className="icon transition-all duration-200 ease-in-out group-hover:translate-x-1">
+                      <IoIosArrowForward />
                     </div>
-                  </Link>
-                )}
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
--- src/app/artworks/page.tsx
+++ src/app/artworks/page.tsx
@@ -1,20 +1,46 @@
 "use client";
 
-import { useState } from "react";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
 import { useFilteredData } from "../hooks/useFilteredData";
+import useWindowWidth from "../hooks/useWindowWidth";
 import ArtworksContainer from "./components/ArtworksContainer";
-import Filter from "./components/Filter";
 
 export default function Artworks() {
-  const [active, setActive] = useState("artworks");
-  const { data } = useFilteredData(active);
+  const { data } = useFilteredData("project");
+  const maxSizeRef = useRef(null);
+
+  const { width } = useWindowWidth();
+
+  useEffect(() => {
+    if (width > 768) {
+      gsap.to(maxSizeRef.current, {
+        delay: 2,
+        fontSize: "8rem",
+        duration: 1.5,
+        ease: "elastic.out(0.5, 0.2)",
+      });
+    } else {
+      gsap.to(maxSizeRef.current, {
+        delay: 2,
+        fontSize: "4rem",
+        duration: 1.5,
+        ease: "elastic.out(0.5, 0.2)",
+      });
+    }
+  }, [width]);
 
   return (
-    <section className="">
-      <div className="mb-10">
-        <Filter active={active} setActive={setActive} />
+    <section className="min-h-screen">
+      <div className="mb-40 flex flex-col items-center justify-center gap-10 lg:gap-20">
+        <div className="tex text-pretty text-center text-4xl leading-tight lg:text-8xl">
+          My vision in <span ref={maxSizeRef}>Maxi size</span>
+        </div>
+        <p className="text-xl text-slate-400">
+          Discover my biggest projects more than 2 meters
+        </p>
       </div>
-      <ArtworksContainer filter={active} item={data} />
+      <ArtworksContainer item={data} />
     </section>
   );
 }
