a6342d2bc21560e6e9c065df6f4c0ef9dcf541ce
rmymartinet <remymartinet.contact@gmail.com>
2024-11-29 21:55:05 +0100

    Refactor: Replace ProductPage with ImageCarousel and ProductDetails for improved structure and functionality

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
-import ProductPage from "@/app/components/Product/ProductPage";
+import { useCart } from "@/app/context/CardContext";
+import useFilteredDataById from "../../hooks/useFilteredDataById";
+import ImageCarousel from "@/app/components/ImagesCarousel";
+import ProductDetails from "@/app/components/Product/ProductDetails";
 import { useFilteredData } from "@/app/hooks/useFilteredData";
+import Image from "next/image";
 
 interface OriginalItemProps {
   params: {
@@ -11,13 +15,37 @@ interface OriginalItemProps {
 
 export default function OriginalItem({ params }: OriginalItemProps) {
   const { id } = params;
-
   const { data } = useFilteredData(id);
-
   const category = data.find((item) => item.id === id)?.category ?? "";
+  const product = useFilteredDataById(id);
+  const { isShoppingOpen } = useCart();
+  if (!product) {
+    // return <SkeletonCard />;
+    return null;
+  }
+
   return (
-    <div>
-      <ProductPage category={category} id={id} />
+    <div className="min-h-screen">
+      <section
+        className={`flex items-start justify-center ${isShoppingOpen ? "opacity-60" : "opacity-100"}`}
+      >
+        <div className="flex flex-col items-center gap-5 md:gap-20">
+          <div className="relative flex h-[80vh] w-screen items-center justify-center">
+            {product && product.imageUrls.length > 2 ? (
+              <ImageCarousel imageUrls={product.imageUrls} />
+            ) : (
+              <Image
+                className="h-full w-full object-contain"
+                src={product?.imageUrls[0] || ""}
+                alt={product?.title || ""}
+                width={5000}
+                height={5000}
+              />
+            )}
+          </div>
+          <ProductDetails category={category} product={product} />
+        </div>
+      </section>
     </div>
   );
 }
