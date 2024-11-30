fac30e747a55ae71f1bf5727d24fe36b49ace811
rmymartinet <remymartinet.contact@gmail.com>
2024-11-30 09:53:19 +0100

    Refactor: Simplify layout and improve responsiveness in ArtworksContainer component

--- src/app/artworks/components/ArtworksContainer.tsx
+++ src/app/artworks/components/ArtworksContainer.tsx
@@ -11,46 +11,39 @@ interface ImagesContainerProps {
 
 const ArtworksContainer = ({ item }: ImagesContainerProps) => {
   return (
-    <section className="flex justify-center">
-      {/*AJOUT DE HEROSUBCONTENT*/}
-      <div className="flex flex-col gap-20 lg:gap-72">
-        {item.map((imgData: Item, id: number) => (
-          <div
-            className="flex max-h-[80vh] max-w-[80vw] flex-col gap-10"
-            key={id}
-          >
-            <Image
-              className="h-full w-full object-contain"
-              width={5000}
-              height={5000}
-              src={imgData.imageUrls[0]}
-              alt="Image"
-            />
-
-            <div className="flex flex-col items-start md:flex-row md:items-center md:justify-between md:gap-0 md:px-10">
-              <div className="flex w-full justify-between gap-6">
-                <div className="md:text-md flex flex-wrap gap-3 lg:text-lg">
-                  <p>{imgData.title}</p>
-                  <span>|</span>
-                  <p>{imgData.dimension}</p>
-                  <span>|</span>
-                  <p>{imgData.date}</p>
-                </div>
-
-                <Link href={`/artworks/${imgData.id}`} className="self-end">
-                  <div className="group flex items-center gap-2 text-blue-500">
-                    <p className="cursor-pointer">Learn more</p>
-                    <div className="icon transition-all duration-200 ease-in-out group-hover:translate-x-1">
-                      <IoIosArrowForward />
-                    </div>
-                  </div>
-                </Link>
+    <div className="flex flex-col items-center gap-40">
+      {item.map((imgData: Item, id: number) => (
+        <div key={id} className="w-max-screen flex flex-col gap-4 md:h-[70vh]">
+          <Image
+            className="h-full w-full cursor-pointer object-contain"
+            width={5000}
+            height={5000}
+            src={imgData.imageUrls[0]}
+            alt="Image"
+          />
+          <div className="flex flex-col items-start md:flex-row md:items-center md:justify-between">
+            <div className="flex w-full justify-between">
+              <div className="flex flex-wrap gap-3">
+                <p>{imgData.title}</p>
+                <span>|</span>
+                <p>{imgData.dimension}</p>
+                <span>|</span>
+                <p>{imgData.date}</p>
               </div>
+
+              <Link href={`/artworks/${imgData.id}`} className="self-end">
+                <div className="group flex items-center gap-2 text-blue-500">
+                  <p className="cursor-pointer">Learn more</p>
+                  <div className="icon transition-all duration-200 ease-in-out group-hover:translate-x-1">
+                    <IoIosArrowForward />
+                  </div>
+                </div>
+              </Link>
             </div>
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      ))}
+    </div>
   );
 };
 
