c3cddd521be0e5d8bb6cc00b08e36ddad50afa70
rmymartinet <remymartinet.contact@gmail.com>
2024-12-01 20:58:57 +0100

    Refactor: Enhance OriginalItem component with zoom functionality and custom cursor for improved user interaction

--- src/app/shop/[id]/page.tsx
+++ src/app/shop/[id]/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import gsap from "gsap";
 import { useCart } from "@/app/context/CardContext";
 import useFilteredDataById from "../../hooks/useFilteredDataById";
-import ImageCarousel from "@/app/components/ImagesCarousel";
 import ProductDetails from "@/app/components/Product/ProductDetails";
-import { useFilteredData } from "@/app/hooks/useFilteredData";
-import Image from "next/image";
+import CustomCursor from "@/app/components/CustomCursor";
+import ZoomedImage from "@/app/components/ZoomedImage";
+import { useZoom } from "@/app/context/ZoomProvider";
+import useHover from "../_Animations/useHover";
+import ImageCarousel from "@/app/components/ImagesCarousel";
 
 interface OriginalItemProps {
   params: {
@@ -15,37 +20,104 @@ interface OriginalItemProps {
 
 export default function OriginalItem({ params }: OriginalItemProps) {
   const { id } = params;
-  const { data } = useFilteredData(id);
-  const category = data.find((item) => item.id === id)?.category ?? "";
   const product = useFilteredDataById(id);
   const { isShoppingOpen } = useCart();
+  const { isZoom, setIsZoom } = useZoom();
+  const zoomContainerRef = useRef(null);
+  const mainImgRef = useRef(null);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const { isHovering, setIsHovering } = useHover();
+
+  useEffect(() => {
+    if (isZoom && zoomContainerRef.current) {
+      gsap.from(zoomContainerRef.current, {
+        opacity: 0,
+        duration: 0.5,
+        ease: "power3.inOut",
+      });
+    }
+  }, [isZoom]);
+
+  useEffect(() => {
+    if (product && product.imageUrls.length > 0 && !hasAnimated) {
+      gsap.from(mainImgRef.current, {
+        opacity: 0,
+        x: 1000,
+        duration: 1.5,
+        ease: "power2.Out",
+      });
+      setHasAnimated(true);
+    }
+  }, [product, hasAnimated]);
+
+  useEffect(() => {
+    if (isHovering) {
+      document.body.style.cursor = "none";
+    } else {
+      document.body.style.cursor = "auto";
+    }
+  }, [isHovering]);
+
   if (!product) {
-    // return <SkeletonCard />;
     return null;
   }
 
+  const handleZoomImage = () => {
+    setIsZoom(true);
+  };
+
   return (
-    <div className="min-h-screen">
+    <>
+      {isZoom && (
+        <ZoomedImage
+          src={product.imageUrls[carouselIndex] || ""}
+          onClose={() => setIsZoom(false)}
+        />
+      )}
+
       <section
-        className={`flex items-start justify-center ${isShoppingOpen ? "opacity-60" : "opacity-100"}`}
+        className={`-mt-[12vh] flex items-center justify-center overflow-hidden px-10 ${isZoom && "blur-md"} ${isShoppingOpen ? "opacity-60" : "opacity-100"}`}
       >
-        <div className="flex flex-col items-center gap-5 md:gap-20">
-          <div className="relative flex h-[80vh] w-screen items-center justify-center">
-            {product && product.imageUrls.length > 2 ? (
-              <ImageCarousel imageUrls={product.imageUrls} />
-            ) : (
-              <Image
-                className="h-full w-full object-contain"
-                src={product?.imageUrls[0] || ""}
-                alt={product?.title || ""}
-                width={5000}
-                height={5000}
+        <div className="lg:grid-cols-customShopId flex flex-col items-center gap-10 px-2 md:px-0 lg:grid">
+          <div className="relative flex items-center justify-center">
+            {product.imageUrls.length > 2 ? (
+              <ImageCarousel
+                mainImgRef={mainImgRef}
+                imageUrls={product.imageUrls}
+                carouselIndex={carouselIndex}
+                setCarouselIndex={setCarouselIndex}
+                isHovering={isHovering}
+                setIsHovering={setIsHovering}
+                setIsZoom={setIsZoom}
               />
+            ) : (
+              <div
+                className="h-[40vh] w-[95vw] overflow-hidden md:h-[70vh] md:w-[90vw] lg:h-[60vh] lg:w-[60vw]"
+                onClick={handleZoomImage}
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
+              >
+                <CustomCursor isHovering={isHovering} />
+                <div ref={mainImgRef} className="opacity-1">
+                  <Image
+                    className="h-full w-full object-contain"
+                    src={product.imageUrls[0] || ""}
+                    alt={product.title || ""}
+                    width={5000}
+                    height={5000}
+                  />
+                </div>
+              </div>
             )}
           </div>
-          <ProductDetails category={category} product={product} />
+          <ProductDetails
+            category={product.category || ""}
+            product={product}
+            hasAnimated={hasAnimated}
+          />
         </div>
       </section>
-    </div>
+    </>
   );
 }
