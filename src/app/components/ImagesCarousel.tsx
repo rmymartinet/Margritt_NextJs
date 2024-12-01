0d59dc5abd86125d70f55d023cc5212fde360ee0
rmymartinet <remymartinet.contact@gmail.com>
2024-12-01 21:06:38 +0100

    Refactor: Update ImageCarousel component with improved props and animation effects

--- src/app/components/ImagesCarousel.tsx
+++ src/app/components/ImagesCarousel.tsx
@@ -1,66 +1,76 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { useRef } from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import useCarousel from "../hooks/useCarousel";
 import Image from "next/image";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import CustomCursor from "./CustomCursor";
+import { ImageCarouselProps } from "@/types/dataTypes";
 
-gsap.registerPlugin(useGSAP);
+const ImageCarousel = ({
+  mainImgRef,
+  imageUrls,
+  carouselIndex,
+  setCarouselIndex,
+  isHovering,
+  setIsHovering,
+  setIsZoom,
+}: ImageCarouselProps) => {
+  const carouselRef = useRef<(HTMLImageElement | null)[]>([]);
 
-const ImageCarousel = ({ imageUrls }: { imageUrls: string[] }) => {
-  const { carouselIndex, handleNextSlide, handlePrevSlide } =
-    useCarousel(imageUrls);
-
-  const prevRef = useRef<HTMLDivElement>(null);
-  const nextRef = useRef<HTMLDivElement>(null);
-
-  const scaleButton = (ref: React.RefObject<HTMLDivElement>) => {
-    const tl = gsap.timeline();
-    tl.to(ref.current, {
-      scale: 1.2,
-      duration: 0.2,
-    }).to(ref.current, {
-      scale: 1,
-      duration: 0.2,
+  useEffect(() => {
+    carouselRef.current.forEach((el, index) => {
+      gsap.fromTo(
+        el,
+        {
+          opacity: 0,
+          x: -1000,
+        },
+        {
+          delay: index * 0.02,
+          opacity: 1,
+          x: 0,
+          duration: 1.5,
+          ease: "power2.Out",
+        },
+      );
     });
-  };
+  }, []);
 
   return (
-    <section className="h-full w-full">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <Image
-        className="h-full w-full object-contain"
-        src={imageUrls[carouselIndex] || ""}
-        alt=""
-        width={5000}
-        height={5000}
-      />
-      {/* Les flèches de navigation positionnées par rapport au conteneur, pas à l'image */}
-      <div className="absolute inset-0 flex items-center justify-between px-5 lg:px-20">
+    <div className="flex flex-col justify-between gap-10">
+      <div className="h-[40vh] w-[95vw] overflow-hidden md:h-[70vh] md:w-[90vw] lg:h-[60vh] lg:w-[60vw]">
+        <CustomCursor isHovering={isHovering} />
         <div
-          ref={prevRef}
-          className="grid h-8 w-8 cursor-pointer place-content-center rounded-full bg-white"
-          onClick={() => {
-            handlePrevSlide();
-            scaleButton(prevRef);
-          }}
-          style={{ userSelect: "none" }}
+          ref={mainImgRef}
+          className="opacity-1 h-full w-full"
+          onClick={() => setIsZoom(true)}
         >
-          <IoIosArrowBack size={20} />
-        </div>
-        <div
-          ref={nextRef}
-          className="grid h-8 w-8 cursor-pointer place-content-center rounded-full bg-white"
-          onClick={() => {
-            handleNextSlide();
-            scaleButton(nextRef);
-          }}
-          style={{ userSelect: "none" }}
-        >
-          <IoIosArrowForward size={20} />
+          <Image
+            className="h-full w-full object-contain"
+            src={imageUrls[carouselIndex] || ""}
+            alt=""
+            width={5000}
+            height={5000}
+            onMouseEnter={() => setIsHovering(true)}
+            onMouseLeave={() => setIsHovering(false)}
+          />
         </div>
       </div>
-    </section>
+      <div className="flex gap-2">
+        {imageUrls.map((item, index) => (
+          <Image
+            ref={(el) => {
+              carouselRef.current[index] = el;
+            }}
+            key={index}
+            className="h-16 w-16 cursor-pointer object-cover md:h-32 md:w-32"
+            src={item || ""}
+            alt=""
+            width={5000}
+            height={5000}
+            onClick={() => setCarouselIndex(index)}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
