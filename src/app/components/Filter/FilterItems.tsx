a935262511571e7cab5afbfa053762e1e78ed396
rmymartinet <remymartinet.contact@gmail.com>
2024-12-01 21:03:21 +0100

    Refactor: Update FilterItems component styling for improved visual consistency

--- src/app/components/Filter/FilterItems.tsx
+++ src/app/components/Filter/FilterItems.tsx
@@ -8,7 +8,7 @@ const FilterItems = ({
 }) => {
   return (
     <span
-      className={`cursor-pointer rounded-full border p-2 shadow-inner ${isActive ? "border-blue-400 bg-blue-400 text-white" : ""}`}
+      className={`cursor-pointer rounded-full border p-2 shadow-inner ${isActive ? "border-blue-200 bg-blue-400 text-white" : ""}`}
     >
       {filterName.charAt(0).toUpperCase() + filterName.slice(1)}
     </span>
