0cea18c954407a8a12b47a76532caf3f2c19c516
rmymartinet <remymartinet.contact@gmail.com>
2024-12-01 20:59:33 +0100

    Refactor: Add CustomCursor component for enhanced mouse interaction with zoom indication

---
+++ src/app/components/CustomCursor.tsx
@@ -0,0 +1,38 @@
+import { CustomCursorProps } from "@/types/dataTypes";
+import { gsap } from "gsap";
+import { use, useEffect, useRef } from "react";
+import { IoMdAdd } from "react-icons/io";
+
+const CustomCursor = ({ isHovering }: CustomCursorProps) => {
+  const cursorRef = useRef(null);
+
+  // Suivre la position de la souris
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const { clientX, clientY } = e;
+      gsap.to(cursorRef.current, {
+        x: clientX,
+        y: clientY,
+      });
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Curseur personnalisé */}
+      <div
+        ref={cursorRef}
+        className={`custom-cursor ${isHovering ? "active" : ""}`}
+      >
+        {isHovering ? "Zoom" : null}
+      </div>
+    </div>
+  );
+};
+
+export default CustomCursor;
