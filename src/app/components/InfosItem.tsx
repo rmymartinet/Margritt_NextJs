6023a1d2cdc94d2cd1215e7f6a6354eb7a4eaa7d
rmymartinet <remymartinet.contact@gmail.com>
2024-12-01 21:03:04 +0100

    Refactor: Update InfosItem component layout and styling for improved readability

--- src/app/components/InfosItem.tsx
+++ src/app/components/InfosItem.tsx
@@ -6,11 +6,9 @@ interface InfosItemProps {
 
 const InfosItem = ({ label, value, className }: InfosItemProps) => {
   return (
-    <div
-      className={`flex w-full flex-row items-center justify-between gap-4 ${className}`}
-    >
+    <div className={`grid-cols-customShopId grid ${className} `}>
       <div className="text-md md:text-xl">{label}</div>
-      <div className="opacity-70">{value}</div>
+      <div className="text-base text-slate-400 md:text-lg">{value}</div>
     </div>
   );
 };
