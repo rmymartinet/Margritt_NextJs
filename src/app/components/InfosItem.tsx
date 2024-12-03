5401e58a1e6bc14504151fd3a9c6f65ec855cf98
rmymartinet <remymartinet.contact@gmail.com>
2024-12-03 17:41:56 +0100

    Refactor: Adjust layout in InfosItem component for improved spacing and responsiveness

--- src/app/components/InfosItem.tsx
+++ src/app/components/InfosItem.tsx
@@ -7,7 +7,7 @@ interface InfosItemProps {
 
 const InfosItem = ({ label, value, className, textColor }: InfosItemProps) => {
   return (
-    <div className={`grid grid-cols-2 gap-2 ${className} `}>
+    <div className={`gap-4 ${!className && "grid grid-cols-2 gap-2"} `}>
       <div className="text-lg md:text-xl">{label}</div>
       <div className={`text-base ${textColor} md:text-lg`}>{value}</div>
     </div>
