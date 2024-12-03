8d07a78e1b7fce7d75f0c00082533f471b970e74
rmymartinet <remymartinet.contact@gmail.com>
2024-12-03 11:30:06 +0100

    Refactor: Update TitleTransition component to use line splitting and add TitleTransitionFromCenter for enhanced animations

--- src/app/components/Animations/TitleTransition.tsx
+++ src/app/components/Animations/TitleTransition.tsx
@@ -63,18 +63,98 @@ export const TitleTransition = ({
 
   useGSAP(() => {
     if (elementRef.current) {
-      const split = new SplitType(elementRef.current, { types: "chars" });
+      const split = new SplitType(elementRef.current, { types: "lines" });
 
-      gsap.from(split.chars, {
+      gsap.from(split.lines, {
         y: yposition,
         duration: 1,
         stagger: 0.03,
         ease: "power2.out",
       });
 
-      return () => gsap.killTweensOf(split.chars);
+      return () => gsap.killTweensOf(split.lines);
     }
   }, [yposition]);
 
   return <div ref={elementRef}>{children}</div>;
+};
+
+// export const TitleTransition = ({ children }: TitleTransitionProps) => {
+//   const elementRef = useRef<HTMLDivElement>(null);
+
+//   const child = elementRef.current?.children;
+
+//   useGSAP(() => {
+//     if (child) {
+//       gsap.from(child, {
+//         delay: 1,
+//         skewX: 30,
+//         y: 400,
+//         duration: 1,
+//         ease: "power2.out",
+//         stagger: 0.1,
+//       });
+//     }
+//   }, []);
+
+//   return (
+//     <div
+//       ref={elementRef}
+//       className="flex w-full flex-col overflow-hidden md:w-[70%] lg:w-[30%]"
+//     >
+//       {children}
+//     </div>
+//   );
+// };
+
+export const TitleTransitionFromCenter = () => {
+  const rightTitleRef = useRef<HTMLHeadingElement>(null);
+  const leftTitleRef = useRef<HTMLHeadingElement>(null);
+
+  useGSAP(() => {
+    if (rightTitleRef.current && leftTitleRef.current) {
+      const rightSplit = new SplitType(rightTitleRef.current, {
+        types: "chars",
+      });
+      const leftSplit = new SplitType(leftTitleRef.current, { types: "chars" });
+
+      gsap.from(rightSplit.chars, {
+        delay: 0.5,
+        y: 400,
+        duration: 1.2,
+        ease: "power2.out",
+        stagger: { amount: 0.2, from: "end" },
+      });
+
+      gsap.from(leftSplit.chars, {
+        delay: 0.5,
+        y: 400,
+        duration: 1.2,
+        ease: "power2.out",
+        stagger: { amount: 0.2 },
+      });
+
+      return () => {
+        gsap.killTweensOf(rightSplit.chars);
+        gsap.killTweensOf(leftSplit.chars);
+      };
+    }
+  }, []);
+
+  return (
+    <div className="flex w-full items-end justify-center overflow-hidden">
+      <h1
+        ref={rightTitleRef}
+        className="text-6xl font-medium uppercase leading-none md:text-9xl lg:text-[18.4vw]"
+      >
+        marg
+      </h1>
+      <h1
+        ref={leftTitleRef}
+        className="text-6xl font-medium uppercase leading-none md:text-9xl lg:text-[18.4vw]"
+      >
+        ritt
+      </h1>
+    </div>
+  );
 };
