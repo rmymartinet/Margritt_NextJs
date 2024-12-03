1a16f167fb48c22c6456a254fc40fb150c764d6a
rmymartinet <remymartinet.contact@gmail.com>
2024-12-03 11:27:07 +0100

    Refactor: Enhance CartSidebar component with quantity management and improved checkout process

--- src/app/components/CartSidebar.tsx
+++ src/app/components/CartSidebar.tsx
@@ -4,17 +4,43 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Image from "next/image";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { useCart } from "../context/CardContext";
 import { useRemoveFromCart } from "../hooks/useRemoveFromCart";
+import QuantitySelector from "./QuantitySelector";
+import { Item } from "@/types/dataTypes";
+import Swal from "sweetalert2";
+import { useFilteredData } from "../hooks/useFilteredData";
+
+gsap.registerPlugin(useGSAP);
 
 const CartSideBar = () => {
-  const { cart, isShoppingOpen, setIsShoppingOpen } = useCart();
+  const { cart, isShoppingOpen, setIsShoppingOpen, updateCartQuantity } =
+    useCart();
+
+  const [isAnimating, setIsAnimating] = useState(false); // État pour gérer l'animation
+
+  const handleAddQuantity = (item: Item) => {
+    if (item.stock && item.tempQuantity < item.stock) {
+      updateCartQuantity(item.id, item.tempQuantity + 1);
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Insufficient Stock",
+        text: "You have reached the maximum stock limit for this item.",
+      });
+    }
+  };
+
+  const handleRemoveQuantity = (item: Item) => {
+    if (item.tempQuantity > 1) {
+      updateCartQuantity(item.id, item.tempQuantity - 1);
+    }
+  };
+
   const removeFromCart = useRemoveFromCart();
-
   const shoppingContainerRef = useRef<HTMLDivElement>(null);
-
   const totalWithoutDelivery = cart.reduce((total, item) => {
     const itemPrice = item?.finalPrice || 0;
     // const itemQuantity = Number(item.quantity) || 0;
@@ -23,36 +49,112 @@ const CartSideBar = () => {
   const totalWithDelivery = totalWithoutDelivery;
   const formattedTotalWithDelivery = totalWithDelivery.toFixed(2);
 
-  console.log(cart);
-
   useGSAP(() => {
-    gsap.set(shoppingContainerRef.current, {
-      x: "200%",
-    });
-    gsap.to(shoppingContainerRef.current, {
-      x: isShoppingOpen ? "0%" : "200%",
-      duration: 0.5,
-      ease: "power3.out",
-    });
+    const element = shoppingContainerRef.current;
+
+    if (!element) return;
 
     if (isShoppingOpen) {
+      setIsAnimating(true);
+      gsap.to(element, {
+        x: "0%",
+        duration: 1,
+        ease: "power2.out",
+        onComplete: () => setIsAnimating(false),
+      });
       document.body.style.overflow = "hidden";
     } else {
+      setIsAnimating(true);
+      gsap.to(element, {
+        x: "200%",
+        duration: 1,
+        ease: "power2.in",
+        onComplete: () => {
+          setIsAnimating(false);
+        },
+      });
       document.body.style.overflow = "auto";
     }
   }, [isShoppingOpen]);
 
+  useGSAP(() => {
+    const element = shoppingContainerRef.current;
+    if (element) {
+      gsap.set(element, { x: "200%" });
+    }
+  }, []);
+
   const handleCloseClick = () => {
-    setIsShoppingOpen(false);
+    if (!isAnimating) {
+      setIsShoppingOpen(false);
+    }
   };
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const { data } = useFilteredData();
+  const allProducts = cart.flat().map((item) => item.id);
+  const compare = data.filter((item) => allProducts.includes(item.id));
+  const outOfStockProduct = compare.find((item) => item.quantity === 0);
+
+  async function checkout() {
+    setLoading(true);
+
+    try {
+      // Vérification du stock
+      if (outOfStockProduct) {
+        Swal.fire({
+          title: "Erreur!",
+          text: `Stock insuffisant pour : ${outOfStockProduct.title} Veillez à retirer cet article de votre panier`,
+          icon: "error",
+          confirmButtonText: "OK",
+        });
+        setLoading(false);
+        throw new Error("Stock insuffisant");
+      }
+
+      // Vérification des prix pour les passer à la session de paiement
+      const products = cart.flat();
+
+      if (products.some((product) => product.price === undefined)) {
+        console.error("Un ou plusieurs produits n'ont pas de prix défini.");
+        setLoading(false);
+        return;
+      }
+
+      // Création de la session de paiement
+      const response = await fetch(`/api/checkout_sessions`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          products,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data?.url) {
+        const url = data.url;
+        setLoading(false);
+        window.location.href = url;
+      }
+    } catch (error) {
+      console.error(
+        "Erreur lors de la création de la session de paiement :",
+        error,
+      );
+      setLoading(false);
+    }
+  }
 
   return (
     <div
       ref={shoppingContainerRef}
-      className="pointer-events-auto fixed right-0 top-0 z-50 flex h-[102dvh] w-[102vw] flex-col overflow-y-auto border-2 bg-white shadow-lg md:h-screen md:w-[400px]"
+      className="pointer-events-auto fixed right-0 top-1/2 z-50 flex h-[100dvh] w-[100vdh] -translate-y-1/2 flex-col overflow-y-auto border-2 bg-white shadow-lg md:right-5 md:h-[80vh] md:w-[450px] md:rounded-xl"
     >
-      <div className="cursor-pointer p-5">
-        <IoClose onClick={handleCloseClick} size={20} />
+      <div className="w-max cursor-pointer rounded-br-xl bg-black p-4">
+        <IoClose onClick={handleCloseClick} size={20} color="white" />
       </div>
       <div className="flex-1 overflow-y-auto p-5">
         {cart.length > 0 ? (
@@ -70,17 +172,24 @@ const CartSideBar = () => {
                 src={item.imageUrls[0]}
                 alt={item.title}
               />
-              <div className="flex-1">
+              <div className="flex flex-col">
                 <h3 className="mb-1 text-sm">{item.title}</h3>
                 <p className="mb-1 text-sm font-bold">{item.finalPrice} €</p>
                 <p className="text-sm">Quantity: {item.tempQuantity}</p>
               </div>
-              <button
-                className="cursor-pointer text-sm text-red-500"
-                onClick={() => removeFromCart(item.id)}
-              >
-                Remove
-              </button>
+              <div className="flex flex-col gap-4">
+                <button
+                  className="cursor-pointer text-sm text-red-500"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  Remove
+                </button>
+                <QuantitySelector
+                  quantity={item.tempQuantity || 1}
+                  onAdd={() => handleAddQuantity(item)}
+                  onRemove={() => handleRemoveQuantity(item)}
+                />
+              </div>
             </div>
           ))
         ) : (
@@ -105,11 +214,20 @@ const CartSideBar = () => {
           <Link href="/checkout">
             <button
               onClick={() => setIsShoppingOpen(false)}
-              className="w-full border border-black bg-white py-2 text-sm uppercase transition hover:bg-black hover:text-white"
+              className="w-full border border-[#4A628A] bg-white py-2 text-sm uppercase transition hover:bg-[#4A628A] hover:text-white"
             >
               Checkout
             </button>
           </Link>
+          <button
+            onClick={() => {
+              setIsShoppingOpen(false);
+              checkout();
+            }}
+            className="w-full border border-[#4A628A] bg-white py-2 text-sm uppercase transition hover:bg-[#4A628A] hover:text-white"
+          >
+            {loading ? "Loading..." : "Order now"}
+          </button>
         </div>
       </div>
     </div>
