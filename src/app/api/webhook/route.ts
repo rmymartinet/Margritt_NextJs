98e702f4799d271cc1969eed7d6b7799d19ed2a9
rmymartinet <remymartinet.contact@gmail.com>
2024-12-15 00:43:45 +0100

    Modification: Change the export of the Stripe instance to a constant for better encapsulation.

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/lib/prisma";
 import { CustomerAddress } from "@/types/dataTypes";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   typescript: true,
 });
 
