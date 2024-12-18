320aa6a715fa7dbca55570fbd1634f9c11a6cdf5
rmymartinet <remymartinet.contact@gmail.com>
2024-12-18 18:20:11 +0100

    Added image handling from new domains and updated dependencies to integrate Uploadthing.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Ajoutez ici le domaine de votre image
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io", // Autorise les images depuis utfs.io
+        pathname: "/**", // Autorise tous les chemins sous utfs.io
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.inery", // Autorise les images depuis cloud.inery
+        pathname: "/**", // Autorise tous les chemins sous cloud.inery
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // Autorise les images depuis res.cloudinary.com
+        pathname: "/**", // Autorise tous les chemins sous res.cloudinary.com
+      },
+    ],
   },
 };
 
